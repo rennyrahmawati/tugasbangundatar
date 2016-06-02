c7464cbda84a8fba51d48e3f1d5b98f71cbf5283
unknown <erlandorico@yahoo.co.id>
2016-06-02 19:13:20 +0700

    tugasbangundatar

---
+++ main.java
@@ -0,0 +1,11 @@
+package bangundatarabstract;
+
+public class main {
+    public static void main(String[] args) {
+        Polymorp poly = new Polymorp();
+        poly.poly(new Persegi());
+        poly.poly(new PersegiPanjang());
+        poly.poly(new Segitiga());
+        
+    }
+}
